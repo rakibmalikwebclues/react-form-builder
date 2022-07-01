8378db055150a08a7fc5558ccc07b7a923dad9c5
rakibmalik <rakib@webcluesinfotech.com>
2022-07-01 19:25:14 +0530

    Added Signup login with firebase.

--- src/App.js
+++ src/App.js
@@ -6,9 +6,11 @@ import {
 
 import Navbar from "./components/Navbar"
 import Footer from "./components/Footer"
+import Login from "./pages/Login"
 
 //Importing pages
 import Home from "./pages/Home"
+import Signup from "./pages/Signup"
 
 function App() {
   return (
@@ -18,6 +20,8 @@ function App() {
           <div className="container main">
             <Switch>
               <Route exact path="/" component={Home} />
+              <Route path="/signup" component={Signup} />
+              <Route path="/login" component={Login} />
             </Switch>
           </div>
         </Router>
